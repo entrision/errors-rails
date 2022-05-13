4414e58ed4ecea91c713ed5f68ae7dd67c7aa3c1
Robert D. Cotey II <rcotey@entrision.com>
2022-05-13 09:55:09 -0400

    Initial commit

---
+++ lib/errors/rails.rb
@@ -0,0 +1,8 @@
+require "errors/rails/version"
+
+module Errors
+  module Rails
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/errors/rails/version.rb
@@ -0,0 +1,5 @@
+module Errors
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
